bce6eef00735eee61ea398b3d3da2a5e422f21fc
Rodrigo Farias <rodrigo.javi@hotmail.com>
2022-04-12 22:20:13 -0300

    DEBUG

--- Cafe/settings.py
+++ Cafe/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mq(hs9x^buk*meigx*sptp9pxa5ad=#2bnnjsmcjsyf61@2p%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
