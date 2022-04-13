1794dab3469a0fe4041f5f1c65655f916df6fbf9
Rodrigo Farias <rodrigo.javi@hotmail.com>
2022-04-12 22:07:32 -0300

    DEBUG

--- Cafe/settings.py
+++ Cafe/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mq(hs9x^buk*meigx*sptp9pxa5ad=#2bnnjsmcjsyf61@2p%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['jeonseol-cafe-blog.herokuapp.com']
 
